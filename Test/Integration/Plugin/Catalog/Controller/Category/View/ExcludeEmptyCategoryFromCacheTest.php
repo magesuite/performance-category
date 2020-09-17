f288079f1d21c168b25dbbf06886c8f2c73dd77b
Paweł Knap <pawel.knap@creativestyle.pl>
2020-09-17 10:43:35 +0200

    - LP-698 - added integration test for not caching empty category plugin

---
+++ Test/Integration/Plugin/Catalog/Controller/Category/View/ExcludeEmptyCategoryFromCacheTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace MageSuite\PerformanceCategory\Test\Integration\Plugin\Catalog\Controller\Category\View;
+
+/**
+ * @magentoDbIsolation enabled
+ * @magentoAppIsolation enabled
+ */
+class ExcludeEmptyCategoryFromCacheTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * @magentoDataFixture Magento/Catalog/_files/category.php
+     * @magentoCache full_page enabled
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
+    public function testEmptyCategoryReturnNoCacheHeaders()
+    {
+        $this->getRequest()->setMethod(\Magento\Framework\App\Request\Http::METHOD_GET);
+        $this->getRequest()->setParam('id', 333);
+        $this->dispatch('/catalog/category/view');
+        $this->assertHeaderPcre('Pragma', '/no-cache/i');
+        $this->assertHeaderPcre('Cache-Control', '/(max-age=0|must-revalidate|no-cache|no-store)/i');
+    }
+
+    /**
+     * @magentoDataFixture Magento/Catalog/_files/category_product.php
+     * @magentoCache full_page enabled
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
+    public function testNotEmptyCategoryReturnNoCacheHeaders()
+    {
+        $this->getRequest()->setMethod(\Magento\Framework\App\Request\Http::METHOD_GET);
+        $this->getRequest()->setParam('id', 333);
+        $this->dispatch('/catalog/category/view');
+        $this->assertHeaderPcre('Pragma', '/cache/i');
+        $this->assertHeaderPcre('Cache-Control', '/(max-age|public)/i');
+    }
+}
