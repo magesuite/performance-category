2d1adb5f96019cee4468d637fc55480f0b6670d1
Piotr Matras <piotr.matras@creativestyle.pl>
2024-10-18 17:49:37 +0200

    feat: KRG-2918 avoid second checking inventory status

---
+++ Plugin/CatalogInventory/Model/ResourceModel/Stock/Status/AddQuantityColumnToSelect.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace MageSuite\PerformanceCategory\Plugin\CatalogInventory\Model\ResourceModel\Stock\Status;
+
+class AddQuantityColumnToSelect
+{
+    protected \Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite;
+    protected \Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface $defaultStockProvider;
+
+    public function __construct(
+        \Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite,
+        \Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface $defaultStockProvider
+    ) {
+        $this->getStockIdForCurrentWebsite = $getStockIdForCurrentWebsite;
+        $this->defaultStockProvider = $defaultStockProvider;
+    }
+
+    public function afterAddStockDataToCollection(
+        \Magento\CatalogInventory\Model\ResourceModel\Stock\Status $subject,
+        $result,
+        $collection,
+        $isFilterInStock
+    ): \Magento\Catalog\Model\ResourceModel\Product\Collection {
+        $stockId = $this->getStockIdForCurrentWebsite->execute();
+
+        if ($stockId === $this->defaultStockProvider->getId()) {
+            $result->getSelect()->columns(['quantity' => 'stock_status_index.qty']);
+        } else {
+            $result->getSelect()->columns(['quantity' => 'stock_status_index.quantity']);
+        }
+
+        return $result;
+    }
+}
