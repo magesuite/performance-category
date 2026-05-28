3b540439a7e264326b0eacb70739bffa9508ce30
Zbigniew Kuraś <zbigniew.kuras@creativestyle.pl>
2026-05-28 22:27:28 +0200

    feat: MGS-5727 Adjust code for php 8.5

--- Plugin/InventoryIndexer/CatalogInventory/Model/Indexer/Stock/CacheCleaner/CleanStockItemCacheStorageAfterCacheClean.php
+++ Plugin/InventoryIndexer/CatalogInventory/Model/Indexer/Stock/CacheCleaner/CleanStockItemCacheStorageAfterCacheClean.php
@@ -25,7 +25,7 @@ class CleanStockItemCacheStorageAfterCacheClean
         }, $records);
 
         foreach ($skus as $sku) {
-            $this->cacheStorage->delete($sku, \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID);
+            $this->cacheStorage->delete(stockId: \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID, sku: $sku);
         }
     }
 }
