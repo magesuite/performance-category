6dabe0a62058a5a0fdfc3010bda4a569b2084dd2
Krzysztof Tomczyk <krzysztof.tomczyk@creativestyle.pl>
2026-06-03 23:37:44 +0200

    Fix: [SCAFA-1237] Tests fix

--- Test/Integration/_files/set_simple_out_of_stock.php
+++ Test/Integration/_files/set_simple_out_of_stock.php
@@ -6,7 +6,7 @@ $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 /** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
 $productRepository = $objectManager->get(\Magento\Catalog\Api\ProductRepositoryInterface::class);
 
-$product = $productRepository->get('simple');
+$product = $productRepository->get('simple', false, null, true);
 $extensionAttributes = $product->getExtensionAttributes();
 $stockItem = $extensionAttributes->getStockItem();
 $stockItem->setIsInStock(false);
